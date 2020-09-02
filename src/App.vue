a63edc99288e438d35cac9e7a7cd1c27cb94f0ba
MaTo <ily4368@gmail.com>
2020-09-02 18:27:28 +0300

    add 4 inputs

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
   <div id="app">
-    <Form/>
+    <Form />
   </div>
 </template>
 
 <script>
-import Form from './components/Form'
+import Form from "./components/Form";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Form
-  }
-}
+    Form,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $success;
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap");
+
+body {
   margin: 0px;
+}
+
+#app {
+  font-family: "Roboto", Arial, Helvetica, sans-serif;
+  background: $bg-dark;
+  width: 100vw;
+  height: 100vh;
+  color: $main-text;
 }
 </style>
