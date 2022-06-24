2ed33bdbc7b940705e2b9cfc76653119f80a40c3
MaTo <ily4368@gmail.com>
2022-06-24 14:37:45 +0300

    Some logic for login form

--- src/components/ui/LoginElement.vue
+++ src/components/ui/LoginElement.vue
@@ -1,12 +1,52 @@
 <script setup lang="ts">
-import { useCssModule } from "vue";
+import { ref, useCssModule } from "vue";
 import Input from "./InputElement.vue";
 const style = useCssModule("login");
+
+const login = ref("");
+const isLoginComplete = ref(false);
+const completeLogin = ref("");
+
+const handleCompleteLogin = () => {
+  completeLogin.value = login.value;
+  login.value = "";
+  isLoginComplete.value = true;
+};
+
+/* const handleLoginComplete = (e: KeyboardEvent) => {}; */
 </script>
 
 <template>
   <div :class="style.form">
-    <Input autofocus :class="style.login" label="Your login" type="login" />
+    <div
+      :class="[
+        style['input-wrapper'],
+        { [style['complete']]: isLoginComplete },
+      ]"
+    >
+      <div :class="style['input-preview-wrapper']">
+        <span :class="style['input-preview']">{{ completeLogin }}</span>
+      </div>
+      <Input
+        :disabled="Boolean(completeLogin)"
+        @keydown.enter.prevent.stop="handleCompleteLogin"
+        v-model="login"
+        autofocus
+        :class="style.login"
+        label="Login"
+        type="login"
+        autocomplete="off"
+      />
+    </div>
+
+    <Input
+      type="password"
+      autocomplete="current-password"
+      v-model="login"
+      autofocus
+      :class="style.password"
+      label="Password"
+    />
   </div>
 </template>
 
@@ -17,11 +57,59 @@ const style = useCssModule("login");
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-wrap: wrap;
+  position: relative;
+  overflow: hidden;
 }
 
-.login {
+.input-wrapper {
+  margin-top: calc(-9.5 * (1vw + 1vh));
+}
+
+.input-wrapper.complete {
+  transform: translateY(calc(-10 * (1vw + 1vh)));
+}
+
+.password {
+  display: none;
+}
+
+.input-wrapper,
+.login,
+.password {
+  position: absolute;
   font-size: calc(3 * (1vw + 1vh));
   width: 80vw;
   min-width: 280px;
+}
+
+.input-preview-wrapper {
+  position: absolute;
+  width: 80vw;
+  box-sizing: border-box;
+}
+
+.input-preview {
+  position: absolute;
+  left: 0;
+  transform: translate(0%, -8%);
+  padding: 1em 1em 1em 1em;
+}
+
+.complete .input-preview {
+  opacity: 1;
+  left: 50%;
+  transform: translate(-50%, 0px);
+}
+
+.input-preview,
+.login,
+.input-wrapper {
+  transition: all 0.5s;
+}
+
+.complete .login {
+  opacity: 0;
+  pointer-events: none;
 }
 </style>
